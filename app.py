6c401579bf62f5b9ceea87c029084fe351cbf73f
AinaKIKISAGBE <aina.kiki.sagbe@gmail.com>
2024-10-26 14:50:18 +0200

    premier depot de flaskapi dd k25

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def welcome():
     return "Hello, I'm Aina KIKI-SAGBE you teacher, this your first test flask"
 
-
+ 
 if __name__=='__main__':
     app.run(host='0.0.0.0', 
 #            port=5000, 
